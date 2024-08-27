09b6df1fbb425a5b74a34d456d3eba16fafbc0a0
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-08-28 00:50:57 +0400

    Update Todo.tsx

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,42 +1,50 @@
-import { useState, useRef } from "react"
-import styles from "./Todo.module.css"
+import { useState, useRef } from "react";
+import styles from "./Todo.module.css";
 
 function Todo() {
-    const list = ["saba", "nini", "bitcamp"]
-    const [box, setBox] = useState([])
-    const [tasks, setTasks] = useState("")
-    const bodyRef = useRef(null)
-    
+    const [box, setBox] = useState([]);
+    const [tasks, setTasks] = useState("");
+    const bodyRef = useRef(null);
 
     function handleInpt(e) {
-        setTasks(e.target.value)
+        setTasks(e.target.value);
     }
 
     function addTask() {
-        setBox([...box, tasks])
-        setTasks("")
-        console.log(box)
+        setBox([...box, tasks]);
+        setTasks("");
+        console.log(box);
         console.log(tasks);
         if (bodyRef.current) {
-            bodyRef.current.style.backgroundColor = "green"
+            bodyRef.current.style.backgroundColor = "green";
         }
     }
 
-  return (
-    <center><div className={styles.box} ref={bodyRef}>
-        <h1>Todo App</h1>
-        <input
-         type="text"
-         name="text1"
-         required onChange={handleInpt} 
-         value={tasks} 
-         className={styles.inpt}
-         /><br /><br />
-        <button onClick={addTask} className={styles.btn}>Add Task</button>
-        <h2>{box.map(t => <h2>{t}</h2>)}</h2>
-
-    </div></center>
-  )
+    return (
+        <center>
+            <div className={styles.box} ref={bodyRef}>
+                <h1>Todo App</h1>
+                <input
+                    type="text"
+                    name="text1"
+                    required
+                    onChange={handleInpt}
+                    value={tasks}
+                    className={styles.inpt}
+                />
+                <br />
+                <br />
+                <button onClick={addTask} className={styles.btn}>
+                    Add Task
+                </button>
+                <div>
+                    {box.map((t, index) => (
+                        <p key={index}>{t}</p>
+                    ))}
+                </div>
+            </div>
+        </center>
+    );
 }
 
-export default Todo
+export default Todo;
